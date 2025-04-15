3b411798703304934ebf465fd05c6dc400f25dca
Winnie Kelley <w642kelley@gmail.com>
2025-04-15 16:38:28 -0500

    dropping and creating db tintamaytoes is done

--- server/database/db.sql
+++ server/database/db.sql
@@ -1,11 +1,15 @@
--- create database and tables
+-- create database
 
--- DROP the database first in case there's already an exisisting database
+-- DROP the database first in case there's an existing one
+DROP DATABASE tintamaytoes;
 
--- CREATE the database now
+-- CREATE the database 
+CREATE DATABASE tintamaytoes;
 
 -- connect to the database
+\connect tintamaytoes;
 
 -- CREATE tables
-
 -- ROUNDS tables
+
+-- FAVORITES tables
