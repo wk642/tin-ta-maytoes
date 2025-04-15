94c5f15c5030cae21da84554a9aaa5e73bfc464c
Winnie Kelley <w642kelley@gmail.com>
2025-04-15 16:37:24 -0500

    psuedo code for creating db and tables

---
+++ server/database/db.sql
@@ -0,0 +1,11 @@
+-- create database and tables
+
+-- DROP the database first in case there's already an exisisting database
+
+-- CREATE the database now
+
+-- connect to the database
+
+-- CREATE tables
+
+-- ROUNDS tables
