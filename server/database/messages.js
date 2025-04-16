1178bd772cff39f337f96733da0df40d81796386
Winnie Kelley <w642kelley@gmail.com>
2025-04-16 01:30:44 -0500

    Psuedo code for api

---
+++ server/database/messages.js
@@ -0,0 +1,11 @@
+const MESSAGES = [
+  {
+    // questions
+      // intro questions
+      // questions after answers
+    // answers
+      // correct answer
+      // incorrect answer
+    // results    
+  }
+]
