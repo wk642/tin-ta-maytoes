ebc198a9dbcb64b9fe2a7b1bacbafba8687b04e3
Winnie Kelley <w642kelley@gmail.com>
2025-04-17 09:14:07 -0500

    Cleaned up assets and switching up icon to animated icon

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,16 @@
 import TinTaMaytoesIcon from './assets/TinTaMaytoesIcon.png'
-import TinTaMaytoesExplanation from './assets/TinTaMaytoesExplanation.mp4'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import TinTaMaytoesExplanation from './assets/videos/TinTaMaytoesExplanation.mp4'
+import playIcon from './assets/icons/playIcon.gif'
+
+// animated icons from "Animated Icons" (https://animatedicons.co/)
 
 export default function App() {
   return (
     <>
       <h1>Tin Ta Maytoes</h1>
-      <FontAwesomeIcon icon={faCirclePlay} />
-      <img src={TinTaMaytoesIcon} alt="Tin Ta Maytoes Icon" className="w-12 h-12" />
-      <video src={TinTaMaytoesExplanation} width="200" height="100" controls> </video> 
+      <img src={ playIcon }/>
+      <img src={ TinTaMaytoesIcon } alt="Tin Ta Maytoes Icon" className="w-12 h-12" />
+      <video src={ TinTaMaytoesExplanation } width="200" height="100" controls> </video> 
     </>
   )
 }
