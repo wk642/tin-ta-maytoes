c1847b280fa3aa779db2310d511db259e12eba6d
Winnie Kelley <w642kelley@gmail.com>
2025-04-17 12:49:28 -0500

    App.jsx is styled now

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,27 @@
-import TinTaMaytoesIcon from './assets/TinTaMaytoesIcon.png'
-import TinTaMaytoesExplanation from './assets/videos/TinTaMaytoesExplanation.mp4'
-import playIcon from './assets/icons/playIcon.gif'
-
-// animated icons from "Animated Icons" (https://animatedicons.co/)
+import TinTaMaytoesIcon from './assets/TinTaMaytoesIcon.png';
+import Explanation from './assets/videos/Explanation.mp4';
+import playIcon from './assets/icons/playIcon.gif';
 
 export default function App() {
   return (
-    <div className="font-cbyg">
-      <h1 className="text-gameTitle">Tin</h1>
-      <h1 className="text-gameTitle">Ta</h1>
-      <h1 className="text-gameTitle">Maytoes</h1>
-      <img src={ playIcon }/>
-      <img src={ TinTaMaytoesIcon } alt="Tin Ta Maytoes Icon" className="w-12 h-12" />
-      <video src={ TinTaMaytoesExplanation } width="200" height="100" controls> </video> 
+    <div className="font-cbyg flex flex-col h-screen bg-orange-300 overflow-hidden">
+      <div className="flex -mt-10">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-gameTitle ml-80 h-60">Tin</h1>
+          <h1 className="text-gameTitle ml-80 -mt-10 mb-10">Ta</h1>
+        </div>
+        <img
+          src={ TinTaMaytoesIcon }
+          alt="Tin Ta Maytoes Icon"
+          className="w-auto h-110 mt-10"
+        />
+        <video src={ Explanation } controls className="ml-4 mt-15 h-110" />
+      </div>
+      
+      <h1 className="text-gameTitle ml-90 -mt-35">Maytoes</h1>
+      <div className="flex justify-center mt-4">
+        <img src={ playIcon } alt="Play Icon" className="-mt-25" />
+      </div>
     </div>
-  )
+  );
 }
