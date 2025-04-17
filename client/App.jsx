bed61c9fe2a96f97fa7dc7bddafd53ee57b89277
Winnie Kelley <w642kelley@gmail.com>
2025-04-16 21:15:41 -0500

    Added icon and set up tailwinds

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,10 @@
+import TinTaMaytoesIcon from './assets/TinTaMaytoesIcon.png'
+
 export default function App() {
   return (
     <>
       <h1>Tin Ta Maytoes</h1>
+      <img src={TinTaMaytoesIcon} alt="Tin Ta Maytoes Icon" className="w-12 h-12" />
     </>
   )
 }
