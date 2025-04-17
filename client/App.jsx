7cdaed642c569084eb01d4583323240593925161
Winnie Kelley <w642kelley@gmail.com>
2025-04-17 08:35:25 -0500

    Added explanation video

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
 import TinTaMaytoesIcon from './assets/TinTaMaytoesIcon.png'
+import TinTaMaytoesExplanation from './assets/TinTaMaytoesExplanation.mp4'
 
 export default function App() {
   return (
     <>
       <h1>Tin Ta Maytoes</h1>
       <img src={TinTaMaytoesIcon} alt="Tin Ta Maytoes Icon" className="w-12 h-12" />
+      <video src={TinTaMaytoesExplanation} width="200" height="100" controls> </video> 
     </>
   )
 }
