f8ed45a20466ccaadfb2a7409ff7e8aef91bad06
Winnie Kelley <w642kelley@gmail.com>
2025-04-17 15:29:59 -0500

    Fixed video size and gif only on hover for play button

--- client/App.jsx
+++ client/App.jsx
@@ -1,28 +1,31 @@
 import TinTaMaytoesIcon from './assets/TinTaMaytoesIcon.png';
-import Explanation from './assets/videos/Explanation.mp4';
-import playIcon from './assets/icons/playIcon.gif';
+import NameExplanation from './assets/videos/NameExplanation.mp4';
+import playIconGif from './assets/icons/playIconGif.gif';
+import playIconStatic from './assets/icons/playIconStatic.png';
 
 export default function App() {
   return (
     <main>
-
       <div className="font-cbyg flex flex-col h-screen bg-orange-300 overflow-hidden">
         <div className="flex -mt-10">
           <div className="flex flex-col justify-center">
             <h1 className="text-gameTitle ml-80 h-60">Tin</h1>
             <h1 className="text-gameTitle ml-80 -mt-10 mb-10">Ta</h1>
           </div>
+
           <img
             src={ TinTaMaytoesIcon }
             alt="Tin Ta Maytoes Icon"
             className="w-auto h-110 mt-10"
           />
-          <video src={ Explanation } controls className="ml-4 mt-15 h-110" />
+          <video src={ NameExplanation } controls className="ml-4 mt-15 h-110" />
         </div>
         
         <h1 className="text-gameTitle ml-90 -mt-35">Maytoes</h1>
-        <div className="flex justify-center mt-4">
-          <img src={ playIcon } alt="Play Icon" className="-mt-25" />
+        <div className="flex justify-center -mt-15 ml-160 relative w-fit h-fit group">
+          <div className="w-20 h-20 bg-contain bg-no-repeat" style={{ backgroundImage: `url('${playIconStatic}')` }}>
+            <div className="absolute w-24 h-24 -inset-2 opacity-0 transition-opacity group-hover:opacity-100 bg-contain bg-no-repeat" style={{ backgroundImage: `url('${playIconGif}')` }}></div>
+          </div>
         </div>
       </div>
     </main>
