9670d1d64bc86b55b66657b539ff40548ff32bae
Winnie Kelley <w642kelley@gmail.com>
2025-04-17 20:33:51 -0500

    Initialize GameMenu.jsx with TODO

---
+++ client/components/GameMenu.jsx
@@ -0,0 +1,12 @@
+// GameMenu.jsx
+/* TODO:
+  - MVP
+    - have a settings icon
+    - have a leaderboards icon
+    - have a home icon
+    - Those three things might be in it's own component
+
+    - Scenario Mode
+    - Tornado Expert Mode
+    - Suggestions
+*/
