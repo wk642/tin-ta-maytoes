0cd0514344521e812c9ecad7ffa14ef2fce00ab3
Winnie Kelley <w642kelley@gmail.com>
2025-04-21 13:01:58 -0500

    Fixed circle before message and started useNavigate

--- src/components/Constants.jsx
+++ src/components/Constants.jsx
@@ -6,3 +6,21 @@
   - leaderboard icon
   - settings icon
 */
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Contants() {
+  return(
+    <>
+      {/* back icon */}
+
+      {/* home icon */}
+
+      {/* leaderboard icon */}
+
+      {/* settings icon */}
+    </>
+  )
+}
--- src/components/GameMenu.jsx
+++ src/components/GameMenu.jsx
@@ -12,6 +12,8 @@
 */
 
 import bgImage from '../assets/bgImage.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
 
 export default function GameMenu() {
   return (
@@ -22,7 +24,7 @@ export default function GameMenu() {
         <div className="flex flex-col w-full items-center">
           <div className="flex items-center w-3/4 mb-4 max-w-[80%] self-start">
             <div className="mr-2 h-12 w-12 rounded-full bg-gray-400 flex items-center justify-center">
-              <UserCircle className="h-8 w-8 text-white" />
+              <FontAwesomeIcon icon={faCircleUser} className="h-8 w-8 text-white" />
             </div>
             <button
               className="text-3xl bg-white/80 text-gray-800 rounded-xl p-4 shadow-md flex-1"
@@ -37,13 +39,13 @@ export default function GameMenu() {
               TORNADO EXPERT
             </button>
             <div className="ml-2 h-12 w-12 rounded-full bg-gray-400 flex items-center justify-center">
-              <UserCircle className="h-8 w-8 text-white" />
+              <FontAwesomeIcon icon={faCircleUser} className="h-8 w-8 text-white" />            
             </div>
           </div>
           <div className="flex items-center w-3/4 max-w-[80%] self-start">
            <div className="mr-2 h-12 w-12 rounded-full bg-gray-400 flex items-center justify-center">
-              <UserCircle className="h-8 w-8 text-white" />
-            </div>
+            <FontAwesomeIcon icon={faCircleUser} className="h-8 w-8 text-white" />            
+          </div>
             <button
               className="text-3xl bg-white/80 text-gray-800 rounded-xl p-4 shadow-md flex-1"
             >
