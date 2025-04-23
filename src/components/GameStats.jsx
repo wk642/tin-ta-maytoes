f734fefaa53c297a041b93c12cde88f1510969dd
Winnie Kelley <w642kelley@gmail.com>
2025-04-23 16:41:27 -0500

    Added GameStats coming sooon and navigation icons

---
+++ src/components/GameStats.jsx
@@ -0,0 +1,29 @@
+// GameStats.jsx 
+/* TODO:
+  - MVP
+    - home button
+    - settings icon
+    - leaderboards icon
+    - Coming soon
+  - Future:
+    - Tableau data
+*/
+
+import HomeIcon from "./HomeIcon";
+import SettingsIcon from "./SettingsIcon";
+
+export default function GameStats() {
+  return (
+    <main>
+      <div className="fixed top-40 flex ml-42 gap-1 justify-content">
+        <HomeIcon />
+        <SettingsIcon />
+      </div>
+
+      {/* Tableau - game stats*/}
+      <div className="fixed top-35 w-90 h-68 bg-slate-300 -mt-13 ml-200">
+        <h1 className=""> Coming soon ...</h1>
+      </div>
+    </main>
+  )
+}
