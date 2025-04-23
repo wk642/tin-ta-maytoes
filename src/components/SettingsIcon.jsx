26885d31100b6999748b98bff78f61de6914148a
Winnie Kelley <w642kelley@gmail.com>
2025-04-23 12:29:00 -0500

    Adding settings icon component

---
+++ src/components/SettingsIcon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import settingsStatic from "../assets/icons/settingsStatic.png";
+import settingsGif from "../assets/icons/settingsGif.gif";
+
+export default function HomeIcon ({ handleSettingsButtonClick }) {
+  return (
+    <div
+      className="flex justify-start -mt-20 -ml-25 relative w-fit h-fit group cursor-pointer"
+      onClick={handleSettingsButtonClick}>
+      <div className="w-8 h-8 mr-26 -mt-8 bg-contain bg-no-repeat relative"
+      style={{ backgroundImage: `url('${settingsStatic}')` }}>
+        <div className="absolute -inset-4 opacity-0 transition-opacity group-hover:opacity-100 bg-contain bg-no-repeat animation-bounce" style={{ backgroundImage: `url('${settingsGif}')` }} />
+      </div>
+    </div>
+  );
+};
