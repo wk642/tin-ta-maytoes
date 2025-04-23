a02c5d71b45128b5a82aa74f7493d48d3ccd4d51
Winnie Kelley <w642kelley@gmail.com>
2025-04-23 16:47:27 -0500

    Added Link

--- src/components/HomeIcon.jsx
+++ src/components/HomeIcon.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import homeStatic from "../assets/icons/homeStatic.png";
 import homeGif from "../assets/icons/homeGif.gif";
-
+import { Link } from "react-router-dom";
 export default function HomeIcon ({ handleHomeButtonClick }) {
   return (
-    <div
+    <Link to="/"
       className="flex justify-start -mt-20 ml-160 relative w-fit h-fit group cursor-pointer"
       onClick={handleHomeButtonClick}>
       <div className="w-8 h-8 mr-26 -mt-8 bg-contain bg-no-repeat relative"
       style={{ backgroundImage: `url('${homeStatic}')` }}>
         <div className="absolute -inset-4 opacity-0 transition-opacity group-hover:opacity-100 bg-contain bg-no-repeat animation-bounce" style={{ backgroundImage: `url('${homeGif}')` }} />
       </div>
-    </div>
+    </Link>
   );
 };
