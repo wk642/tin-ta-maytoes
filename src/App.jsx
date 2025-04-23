611ae81c4c64f93bd7a6f12c382ce3d5ef1cdc8f
Winnie Kelley <w642kelley@gmail.com>
2025-04-23 16:51:15 -0500

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import GameLayout from './components/GameLayout'; 
+import GameMenu from './components/GameMenu';
+import GamePlay from './components/GamePlay';
+import GameStats from './components/GameStats';
+import Settings from './components/Settings';
+
 
 export default function App () {
   return (
@@ -10,6 +15,10 @@ export default function App () {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gameLayout" element={<GameLayout />} /> 
+          <Route path="/gameMenu" element={<GameMenu />} />
+          <Route path="/gamePlay" element={<GamePlay />} />
+          <Route path="/gameStats" element={<GameStats />} />
+          <Route path="/Settings" element={<Settings />} />
         </Routes>
       </div>
     </BrowserRouter>
