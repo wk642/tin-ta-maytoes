ad92c0e7f25d70c464ba24797ac6774ac301b40e
Winnie Kelley <w642kelley@gmail.com>
2025-04-23 18:04:05 -0500

    removed iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from "./components/Home";
 import GameLayout from './components/GameLayout'; 
 import GameMenu from './components/GameMenu';
