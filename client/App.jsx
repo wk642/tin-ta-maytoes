5aefc1b9fefd731a0370b91fdeb58171fd4d4bf9
Winnie Kelley <w642kelley@gmail.com>
2025-04-25 09:13:53 -0500

    rearranging

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link} from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from "./components/Home";
 import GameLayout from './components/GameLayout';
 import GameMenu from './components/GameMenu';
---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  // root: path.resolve(__dirname, 'client'), // set root to your client folder
+  resolve: {
+    alias: {
+      // src: path.resolve(__dirname, 'client'), // so 'src/' points to 'client/'
+    },
+  },
+  build: {
+    // outDir: path.resolve(__dirname, 'dist'), // render will be putting docs into dist, set dist
+    emptyOutDir: true,
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
