2cf4a2e565efee0d4adae696b3cd88b7da6189ca
Winnie Kelley <w642kelley@gmail.com>
2025-04-24 22:14:57 -0500

    Found another router-dom to delete

--- src/components/SettingsIcon.jsx
+++ src/components/SettingsIcon.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import settingsStatic from "../assets/icons/settingsStatic.png";
 import settingsGif from "../assets/icons/settingsGif.gif";
 
