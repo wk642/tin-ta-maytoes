d0af73cb3dac1996a34b4b778a46d2f46c3ec887
Winnie Kelley <w642kelley@gmail.com>
2025-04-28 12:12:08 -0500

    Added back button to settings page

--- client/components/Settings.jsx
+++ client/components/Settings.jsx
@@ -8,9 +8,12 @@
 
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router";
 import LightMode from "../assets/icons/LightMode.gif";
 import VolumeUp from "../assets/icons/VolumeUp.gif";
 import VolumeDown from "../assets/icons/VolumeDown.gif";
+import backGif from "../assets/icons/backGif.gif";
+import backStatic from "../assets/icons/backStatic.png";
 
 export default function Settings() {
   // states
@@ -26,6 +29,13 @@ export default function Settings() {
   return (
     <main>
       <header className="bg-slate-500/40 items-center justify-content mt-11  w-102 h-18 ml-57">
+        {/* Back button */}
+        <Link to="/gameMenu" className="flex justify-start ml-3 relative w-fit h-fit group cursor-pointer">
+          <div className="w-12 h-12 mt-4 ml-2 bg-contain bg-no-repeat group-hover:animate-bounce" style={{ backgroundImage: `url('${backStatic}')` }}>
+            <div className="absolute -inset-4 opacity-0 transition-opacity group-hover:opacity-100 bg-contain bg-no-repeat" style={{ backgroundImage: `url('${backGif}')` }} />
+          </div>
+        </Link>
+
         <h1 className="text-4xl fixed mt-3 ml-34 justify-content">Settings</h1>
       </header>
 
