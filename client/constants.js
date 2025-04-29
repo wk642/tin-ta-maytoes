c8edae3d5e4fb8a07b6a13ecfb9f84ab31d36bb8
Winnie Kelley <w642kelley@gmail.com>
2025-04-29 14:29:17 -0500

    Switch to localhost

--- client/constants.js
+++ client/constants.js
@@ -1,4 +1,4 @@
 export default { 
-  // serverURL: "http://localhost:5000"
-  serverURL: "https://tintamaytoesapi.onrender.com"
+  serverURL: "http://localhost:5000"
+  // serverURL: "https://tintamaytoesapi.onrender.com"
 };
