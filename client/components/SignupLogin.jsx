ba875d7242d1544d1e226fbcc7c629c2c334191b
Winnie Kelley <w642kelley@gmail.com>
2025-05-01 09:44:14 -0500

    Added tabs and guest links to gamemenu

--- client/components/SignupLogin.jsx
+++ client/components/SignupLogin.jsx
@@ -15,6 +15,54 @@ export default function SignupLogin ({isSignupLoginOpen, closeSignupLogin}) {
           <CloseIcon />
         </button>
         <h2 className="text-2xl font-bold -mt-10 mb-4 text-center">Sign Up | Log In</h2>
+
+      {/* Tab Navigation */}
+      <div className="flex justify-between mb-10 pl-2 pr-2 pt-2 bg-[#FFECDB]">
+          <button
+            className={`px-2 py-2 w-30 rounded-md ${activeTab === 'register' ? 'bg-[#60B5FF] text-white border-b-4' : 'bg-[#AFDDFF] text-[#FF9149] hover:text-white border-b-4'}`}
+            onClick={() => setActiveTab('register')}
+          > 
+            Register
+          </button>
+          <button
+            className={`px-2 py-2 w-30 rounded-md ${activeTab === 'login' ? 'bg-[#60B5FF] text-white border-b-4' : 'bg-[#AFDDFF] text-[#FF9149] hover:text-white border-b-4'}`}
+            onClick={() => setActiveTab('login')}
+          >
+            Login
+          </button>
+          <button
+            className={`px-2 py-2 w-30 rounded-md ${activeTab === 'guest' ? 'bg-[#60B5FF] text-white border-b-4' : 'bg-[#AFDDFF] text-[#FF9149] hover:text-white border-b-4'}`}
+            onClick={() => setActiveTab('guest')}
+          >
+            Play as Guest
+          </button>
+        </div>
+
+        {/* Tab Content */}
+        {activeTab === 'register' && (
+          <div className="flex flex-col space-y-4">
+            <Link to="/" className="bg-[#AFDDFF] hover:bg-[#60B5FF] text-[#FF9149] hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
+              Sign Up
+            </Link>
+          </div>
+        )}
+
+        {activeTab === 'login' && (
+          <div className="flex flex-col space-y-4">
+            <Link to="/" className="bg-[#AFDDFF] hover:bg-[#60B5FF] text-[#FF9149] hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
+              Log In
+            </Link>
+          </div>
+        )}
+
+        {activeTab === 'guest' && (
+          <div className="flex flex-col space-y-4">
+            <Link to="/gameMenu" 
+              className="bg-[#AFDDFF] hover:bg-[#60B5FF] text-[#FF9149] hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
+              Play as Guest
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
