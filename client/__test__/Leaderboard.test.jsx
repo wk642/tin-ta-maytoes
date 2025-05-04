345922f30a3da29ac367ddf18de3e495d23dd7a4
Winnie Kelley <w642kelley@gmail.com>
2025-05-04 16:55:08 -0500

    Leaderboard test

--- client/__test__/Leaderboard.test.jsx
+++ client/__test__/Leaderboard.test.jsx
@@ -1,6 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import Leaderboard from "../components/Leaderboard";
+
+// jest is giving me the runaround with react-router, so going to mock it
+// Mock react-router first 
+jest.mock('react-router', () => ({
+  // Replace the link with a mock
+  Link: ({ children, to, ...props }) => (
+    // replace the link with <a> for now and renders what's inside and the route
+    <a href={to} {...props} data-testid={`nav-link-${to.replace('/', '')}`}>
+      {children}
+    </a>
+  ),
+}));
 
 describe("Leaderboard Component", () => {
   it("renders without crashing", () => {
