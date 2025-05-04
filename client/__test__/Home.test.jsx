6180542c1262b8d47c5e38ddf5d88f1daa8a5dcd
Winnie Kelley <w642kelley@gmail.com>
2025-05-03 23:27:36 -0500

    Updated tests for home

--- client/__test__/Home.test.jsx
+++ client/__test__/Home.test.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
-import Home from '../components/Home';
+import Home from "../components/Home";
 
-jest.mock('../components/SignupLogin', () => () => <div>Mocked</div>);
+// jest is giving me the runaround with react-router, so going to mock it
+// Mock react-router first 
+jest.mock('react-router', () => ({
+  // Replace the link with a mock
+  Link: ({ children, to, ...props }) => (
+    // replace the link with <a> for now and renders what's inside
+    // also replacing the route
+    <a href={to} {...props} data-testid={`nav-link-${to.replace('/', '')}`}>
+      {children}
+    </a>
+  ),
+}));
 
-describe('Homepage ', () => {
+describe("Home Component", () => {
+  it("renders without crashing", () => {
+    render(<Home />);
+  });
+
   it('renders the "Tin Ta Maytoes" title', () => {
-    // Arrange
     render(<Home />);
 
-    // Assert
-    expect(screen.getByText('Tin')).toBeInTheDocument();
-    expect(screen.getByText('Ta')).toBeInTheDocument();
-    expect(screen.getByText('Maytoes')).toBeInTheDocument();
+    const headerText1 = screen.getByRole('heading', { name: /Tin/i });
+    const headerText2 = screen.getByRole('heading', { name: /Ta/i });
+    const headerText3 = screen.getByRole('heading', { name: /Maytoes/i });
+    expect(headerText1).toBeInTheDocument();
+    expect(headerText2).toBeInTheDocument();
+    expect(headerText3).toBeInTheDocument();
   });
 });
