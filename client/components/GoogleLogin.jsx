bec7f772f8f6168342a898ac5660d1f9b9690fb3
Winnie Kelley <w642kelley@gmail.com>
2025-06-08 21:56:19 -0500

    google login

--- client/components/GoogleLogin.jsx
+++ client/components/GoogleLogin.jsx
@@ -1,11 +1,6 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-//client ID : 132242362761-cp87epcf24oivlvtrt9tb0r70jbgkprd.apps.googleusercontent.com
-
 import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
 
-export default function GoogleLanding() { 
+export function GoogleLanding() { 
   return (
     <>
       {/* renders the google login form */}
@@ -13,7 +8,6 @@ export default function GoogleLanding() {
         // If successfully logged in 
         onSuccess={(loginInfo) =>{
           console.log(loginInfo);
-          console.log(jwtDecode(loginInfo.credential));
         }}
         //If not successfully logged in
         onError={() => alert("Login Failed")}
