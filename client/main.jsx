7812229676c4b07c2549ef2412231d21df42437b
Winnie Kelley <w642kelley@gmail.com>
2025-06-10 20:53:46 -0500

    Removed client id section, it keeps breaking it

--- client/main.jsx
+++ client/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+// import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
-import "dotenv/config";
+// import "dotenv/config";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      console.log(clientId);
+    {/* <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}> */}
+      {/* console.log(clientId); */}
       <App />
-    </GoogleOAuthProvider>
+    {/* </GoogleOAuthProvider> */}
   </StrictMode>,
 )
